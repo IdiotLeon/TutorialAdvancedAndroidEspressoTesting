10adf29b8090871436b08a8190eaacdc4675eb36
Yang Lu (Leon) <yanglyu.leon.7@gmail.com>
2019-05-08 14:38:25 -0700

    start: Setting up the project

--- app/src/main/java/com/idiotleon/tutorialadvancedandroidespressotesting/MainActivity.kt
+++ app/src/main/java/com/idiotleon/tutorialadvancedandroidespressotesting/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.idiotleon.tutorialadvancedandroidespressotesting
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_CODE_IEADS = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goToIdeaActivity(v: View) {
+        val btn = v as Button
+
+        val intent = Intent(this, IdeasActivity::class.java)
+        intent.putExtra(IdeasActivity.KEY_THEME, btn.text)
+
+        startActivityForResult(intent, REQUEST_CODE_IEADS)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            REQUEST_CODE_IEADS -> {
+                (et_name as TextView).text = data?.getStringExtra(IdeasActivity.KEY_NAME)
+            }
+            else -> super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
